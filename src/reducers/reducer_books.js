dcb30e933bbc6689905770266e92269311ea20ab
Eric Taylor <vigne6@gmail.com>
2018-09-29 18:48:43 -0700

    base application start

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'My cat and me'},
+        {title: 'My cat and a dog'},
+        {title: 'My cat in a box'},
+        {title: 'Just my cat that is it'}
+    ]
+};
